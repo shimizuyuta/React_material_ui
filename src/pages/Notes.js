916464bb4886525447def8293266a0d557111ff5
clean_water <shmizuyuta213@icloud.com>
2021-10-13 01:30:31 +0900

    作成したnoteを表示

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React , { useEffect,useState } from 'react'
+import {Grid,Paper,Container} from '@material-ui/core'
 
 export default function Notes() {
+  const [notes, setNotes] = useState([])  
+
+  useEffect(() => {
+    fetch('http://localhost:8000/notes')
+      .then((res)=>{
+        return res.json()
+      })
+      .then((data)=>{
+        console.log('data',data)
+        setNotes(data)
+      })
+   
+  }, [])
+
   return (
-    <div>
+    <Container>
       Notes page
-    </div>
+      <Grid container>
+      {notes.map(note => (
+          <Grid item xs={12} md={6} lg={4} key={note.id}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+
   )
 }
