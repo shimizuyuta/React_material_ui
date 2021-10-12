43ffdfcbec0d58a61a9bdc65da67dcaf790e9767
clean_water <shmizuyuta213@icloud.com>
2021-10-13 01:31:11 +0900

    テーマ作成

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import {ThemeProvider,createTheme} from '@material-ui/core'
+
+
+const theme = createTheme({
+  palette:{
+    primary:{
+      main:"#fefefe"
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#010',
+    },
+    typography:{
+      fontFamily:'Quicksand',
+    }
+  }
+})
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
     </Router>
+    </ThemeProvider>
+
   );
 }
 
